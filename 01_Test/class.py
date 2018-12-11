1e8edbacd6a335e0ba3c36b1f80a16e98473413e
mechiz <kaehono@naver.com>
2018-12-11 09:26:42 +0900

    test_class_1

--- 01_Test/class.py
+++ 01_Test/class.py
@@ -1,1 +1,23 @@
-class
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+        return self.value
+
+
+class Calculator:
+    def __init__(self, init_value):
+        self.value = init_value
+
+    def add(self, val):
+        self.value += val
+
+cal = Calculator(0)
+cal.add(3)
+cal.add(4)
+
+print(cal.value)
+
+
